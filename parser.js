4104520a9694d6528ab8599d1824225590a82744
Evelina Ayrapetova <evelina.airapetova97@yandex.ru>
2025-09-10 19:42:17 +0300

    определила основные функции

--- parser.js
+++ parser.js
@@ -1,5 +1,28 @@
-// @todo: напишите здесь код парсера
+// Разбор данных meta
+function parseMeta() {}
 
+function parseMetaKeywords() {}
+
+function parseMetaOpengraph() {}
+
+// Разбор данных product
+function parseProduct() {}
+
+function parseProductTags() {}
+
+function parseProductProperties() {}
+
+function parseProductiImages() {}
+
+// Разбор данных suggested
+// ------побочные функции ещё не определила
+function parseSuggested() {}
+
+// Разбор данных reviews
+// ------побочные функции ещё не определила
+function parseReviews() {}
+
+// Главная функция
 function parsePage() {
   return {
     meta: {},
