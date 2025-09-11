91e00262ae2402f15b1f37041504e7840efe52bd
Evelina Ayrapetova <evelina.airapetova97@yandex.ru>
2025-09-11 16:33:24 +0300

    мета-информация страницы выводится в консоль

--- parser.js
+++ parser.js
@@ -1,9 +1,49 @@
+/**
+ * Функция для разбиения строки на части по определенному
+ * символу для преобразования в массив
+ * +чистка от ненужных пробелов
+ * @param {string} str - строка, которую нужно разделить по символу
+ * @param {string} symbol - символ, по коротому нужно разделить
+ * @returns массив с  элементами без пробелов в начале и конце
+ */
+function splitAndClean(str, symbol) {
+  return str.split(symbol).map((el) => el.trim());
+}
+
 // Разбор данных meta
-function parseMeta() {}
+function parseMeta() {
+  return {
+    title: document.querySelector("title").textContent,
+    description: document
+      .querySelector("meta[name='description']")
+      .getAttribute("content"),
+    keywords: parseMetaKeywords(),
+    language: document.querySelector("html").getAttribute("lang"),
+    opengraph: parseMetaOpengraph(),
+  };
+}
 
-function parseMetaKeywords() {}
+function parseMetaKeywords() {
+  let keywords = document
+    .querySelector("meta[name='keywords']")
+    .getAttribute("content");
+  return splitAndClean(keywords, ",");
+}
 
-function parseMetaOpengraph() {}
+function parseMetaOpengraph() {
+  let rez = {
+    title: document
+      .querySelector("meta[property='og:title']")
+      .getAttribute("content"),
+    image: document
+      .querySelector("meta[property='og:image']")
+      .getAttribute("content"),
+    type: document
+      .querySelector("meta[property='og:type']")
+      .getAttribute("content"),
+  };
+  return rez;
+}
 
 // Разбор данных product
 function parseProduct() {}
@@ -25,7 +65,7 @@ function parseReviews() {}
 // Главная функция
 function parsePage() {
   return {
-    meta: {},
+    meta: parseMeta(),
     product: {},
     suggested: [],
     reviews: [],
